eb0b75ab688eccb34a2e9f89a09e08702d153443
Akila-Piumal <akilapiumal16@gmail.com>
2022-08-12 02:07:19 +0530

    Client_Handler Constructor added and dataOutputStream and dataInputStream Initialized.

--- src/Client_Handler.java
+++ src/Client_Handler.java
@@ -1,4 +1,25 @@
-public class Client_Handler implements Runnable{
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Client_Handler implements Runnable {
+
+    public static ArrayList<Client_Handler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+
+    public Client_Handler(Socket socket){
+        try{
+            this.socket = socket;
+            this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            this.dataInputStream = new DataInputStream(socket.getInputStream());
+        }catch (IOException e){
+
+        }
+    }
 
     @Override
     public void run() {
