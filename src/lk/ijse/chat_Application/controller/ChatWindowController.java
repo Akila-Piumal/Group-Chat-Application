8750510c231f952fd8466c329029f91b6727019d
Akila-Piumal <akilapiumal16@gmail.com>
2022-08-12 01:29:05 +0530

    Chat Window Form Added

---
+++ src/lk/ijse/chat_Application/controller/ChatWindowController.java
@@ -0,0 +1,17 @@
+package lk.ijse.chat_Application.controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class ChatWindowController {
+
+    public Label txtClientName;
+    public TextField textMessage;
+    public TextArea textArea;
+
+    public void sendOnAction(MouseEvent mouseEvent) {
+
+    }
+}
