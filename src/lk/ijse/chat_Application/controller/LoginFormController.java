50876ed83d2264f390ceab8bbd9221f86c278aad
Akila-Piumal <akilapiumal16@gmail.com>
2022-08-12 01:21:58 +0530

    Content Updated

--- src/lk/ijse/chat_Application/controller/LoginFormController.java
+++ src/lk/ijse/chat_Application/controller/LoginFormController.java
@@ -2,12 +2,13 @@ package lk.ijse.chat_Application.controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class LoginFormController {
     public TextField txtUserName;
+    public AnchorPane loginFormContext;
 
     public void btnLoginOnAction(ActionEvent actionEvent) {
-
 
     }
 }
