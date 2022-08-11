0aa40b2b60e2da1b7789765b5166d3539f03d8c2
Akila-Piumal <akilapiumal16@gmail.com>
2022-08-12 01:54:57 +0530

    Server Socket added

---
+++ src/Server.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(6000);
+        Server server = new Server(serverSocket);
+    }
+}
